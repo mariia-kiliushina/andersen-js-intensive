237d64df09185259c7d7b5259ab5a8f908dfb67f
Mariia Kiliushina <markil@Mariias-MacBook-Air.local>
2022-06-18 10:09:46 +0400

    add 3d task solution

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+const makeObjectDeepCopy = (objectToCopyFrom) => {
+  let objectToArray = Object.entries(objectToCopyFrom);
+
+  const objectDeepCopy = objectToArray.reduce((acc, [key, value]) => {
+    const makeArrayDeepCopy = (value) => {
+      value.reduce((acc, item) => {
+        if (Array.isArray(item)) {
+          acc.push(makeArrayDeepCopy(item));
+        } else if (typeof item === 'object') {
+          acc.push(makeObjectDeepCopy(value));
+        } else {
+          acc.push(item);
+        }
+        return acc;
+      }, []);
+      return value;
+    };
+
+    if (Array.isArray(value)) {
+      console.log('array value');
+      console.log(value);
+      acc[key] = makeArrayDeepCopy(value);
+      console.log('ACCUMULATOR after array');
+      console.log(acc);
+      console.log('ACCUMULATOR key after array');
+      console.log(acc[key]);
+    } else if (typeof value === 'object') {
+      acc[key] = makeObjectDeepCopy(value);
+    } else {
+      acc[key] = value;
+    }
+
+    return acc;
+  }, {});
+
+  return objectDeepCopy;
+};
+
+const mainObj = {
+  a: 'test',
+  b: {
+    c: 777,
+    k: { l: 34 },
+  },
+  d: ['foo', ['bar', 'baz'], { 1: 'one', 2: 'two' }],
+};
+
+const deepCopy = makeObjectDeepCopy(mainObj);
+console.log('deepCopy');
+console.log(deepCopy);
+
+deepCopy.b.c = 'CHANGES C';
+deepCopy.b.k.l = 'CHANGES L';
+deepCopy.d[0] = ['changes D'];
+deepCopy.d[1][0] = 'changes bar';
+
+mainObj.b.c = 'again the same C';
+mainObj.b.k.l = 'again the same L';
+mainObj.d[0] = ['again the same D'];
+mainObj.d[1][0] = 'again the same bar';
+
+console.log(deepCopy);
+console.log(mainObj);
+
+//////
 const selectFromInterval = (arrayOfNums, start, end) => {
   if (!Array.isArray(arrayOfNums))
     throw new Error('Ошибка: первый аргумент должен быть массивом');
@@ -34,66 +99,48 @@ const selectFromInterval = (arrayOfNums, start, end) => {
   console.log(slicedArray);
 };
 
-// const makeObjectDeepCopy = (objectToCopyFrom) => {
-//   let objectToArray = Object.entries(objectToCopyFrom);
+//////
+const myIterable = { from: 8, to: 7 };
 
-//   const objectDeepCopy = objectToArray.reduce((acc, [key, value]) => {
-//     const makeArrayDeepCopy = (value) => {
-//       value.reduce((acc, item) => {
-//         if (Array.isArray(item)) {
-//           acc.push(makeArrayDeepCopy(item));
-//         } else if (typeof item === 'object') {
-//           acc.push(makeObjectDeepCopy(value));
-//         } else {
-//           acc.push(item);
-//         }
-//         return acc;
-//       }, []);
-//       return value;
-//     };
+myIterable[Symbol.iterator] = function () {
+  return {
+    iteratorFrom: this.from,
+    iteratorTo: this.to,
+    next() {
+      if (this.iteratorFrom === undefined)
+        throw Error(
+          'Ошибка: у итерируемого объекта отсутствует начальное значение'
+        );
+      if (this.iteratorTo === undefined)
+        throw Error(
+          'Ошибка: у итерируемого объекта отсутствует конечное значение'
+        );
+      if (typeof this.iteratorFrom !== 'number')
+        throw Error(
+          'Ошибка: значения интервалов должны являться числами. В качестве начала интервала получено нечисловое значение'
+        );
+      if (typeof this.iteratorTo !== 'number')
+        throw Error(
+          'Ошибка: значения интервалов должны являться числами. В качестве конца интервала получено нечисловое значение'
+        );
+      if (this.iteratorTo < this.iteratorFrom)
+        throw Error(
+          'Ошибка: значение конца интервала не может быть меньше значения начала интервала'
+        );
 
-//     if (Array.isArray(value)) {
-//       console.log('array value');
-//       console.log(value);
-//       acc[key] = makeArrayDeepCopy(value);
-//       console.log('ACCUMULATOR after array');
-//       console.log(acc);
-//       console.log('ACCUMULATOR key after array');
-//       console.log(acc[key]);
-//     } else if (typeof value === 'object') {
-//       acc[key] = makeObjectDeepCopy(value);
-//     } else {
-//       acc[key] = value;
-//     }
+      if (this.iteratorFrom <= this.iteratorTo) {
+        const result = {
+          done: false,
+          value: this.iteratorFrom++,
+        };
+        return result;
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
 
-//     return acc;
-//   }, {});
-
-//   return objectDeepCopy;
-// };
-
-// const mainObj = {
-//   a: 'test',
-//   b: {
-//     c: 777,
-//     k: { l: 34 },
-//   },
-//   d: ['foo', ['bar', 'baz'], { 1: 'one', 2: 'two' }],
-// };
-
-// const deepCopy = makeObjectDeepCopy(mainObj);
-// console.log('deepCopy');
-// console.log(deepCopy);
-
-// deepCopy.b.c = 'CHANGES C';
-// deepCopy.b.k.l = 'CHANGES L';
-// deepCopy.d[0] = ['changes D'];
-// deepCopy.d[1][0] = 'changes bar';
-
-// mainObj.b.c = 'again the same C';
-// mainObj.b.k.l = 'again the same L';
-// mainObj.d[0] = ['again the same D'];
-// mainObj.d[1][0] = 'again the same bar';
-
-// console.log(deepCopy);
-// console.log(mainObj);
+for (let item of myIterable) {
+  console.log(item);
+}
