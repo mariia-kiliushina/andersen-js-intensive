0384dc9da461b0c1377eda7d125a63a70ed8101a
Mariia Kiliushina <markil@Mariias-MacBook-Air.local>
2022-06-22 17:16:05 +0400

    add: solution of the second task

--- index.js
+++ index.js
@@ -29,30 +29,31 @@ let filtered = array.myFilter(filterCallback);
 
 const createDebounceFunction = (callback, delay) => {
   console.time('debounce');
-  console.time('debAgain');
-
   let timeout;
 
-  const set = () => {
-    console.log('set timeout');
-    console.timeLog('debAgain');
-    timeout = setTimeout(callback, delay);
+  return function () {
+    console.time('debAgain');
+    const set = () => {
+      console.log('set timeout');
+      console.timeLog('debAgain');
+      timeout = setTimeout(callback, delay);
+    };
+    const stop = () => {
+      console.log('stop timeout');
+      console.timeLog('debAgain');
+      clearTimeout(timeout);
+    };
+    stop();
+    set();
+    console.timeEnd('debAgain');
   };
-
-  const stop = () => {
-    console.log('stop timeout');
-    console.timeLog('debAgain');
-    clearTimeout(timeout);
-  };
-
-  set();
-  stop();
-  set();
-  console.timeEnd('debAgain');
 };
 
 const logger = () => {
   console.log('logger invoked');
   console.timeEnd('debounce');
 };
-createDebounceFunction(logger, 2000);
+const debounceFunction = createDebounceFunction(logger, 2000);
+debounceFunction();
+setTimeout(debounceFunction, 1000);
+setTimeout(debounceFunction, 1500);
