cd4b6f0f86154d02cd15570e43f4ab41e969e467
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-06-29 00:07:20 +0400

    add: public class methods

--- stack.js
+++ stack.js
@@ -1,16 +1,56 @@
 class Stack {
-  constructor(numOfElements = 10) {
+  constructor(maxNumOfElements = 10) {
     if (
-      typeof numOfElements !== 'number' ||
-      numOfElements === Infinity ||
-      numOfElements === -Infinity ||
-      numOfElements === 0
+      typeof maxNumOfElements !== 'number' ||
+      maxNumOfElements === Infinity ||
+      maxNumOfElements === -Infinity ||
+      maxNumOfElements === 0
     ) {
       throw new Error('Invalid argument was provided');
     }
+    this.maxNumOfElements = maxNumOfElements;
+    this.stackSize = 0;
+    this.stack = {};
+  }
 
-    this.numOfElements = numOfElements;
+  push(elem) {
+    if (this.stackSize >= this.maxNumOfElements) {
+      throw new Error('Stack is already full. No elements can be pushed');
+    }
+    this.stackSize += 1;
+    this.stack[this.stackSize] = elem;
+  }
+  pop() {
+    let answer = this.stack[this.stackSize];
+    delete this.stack[this.stackSize];
+    this.stackSize -= 1;
+    return answer;
+  }
+  peek() {
+    if (this.stackSize === 0) {
+      return null;
+    }
+    return this.stack[this.stackSize];
+  }
+  isEmpty() {
+    if (this.stackSize === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  toArray() {
+    return Object.values(this.stack);
   }
 }
 
+let myStack = new Stack(3);
+myStack.push('a');
+myStack.push('b');
+myStack.push('c');
+console.log(myStack);
+console.log(myStack.pop());
+console.log(myStack.pop());
+myStack.push('hehe');
+console.log(myStack.peek());
 module.exports = { Stack };
