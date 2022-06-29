3010b1dbaa831a10ef1f0f759ac07b33c26b1a08
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-06-29 23:25:07 +0400

    add toArray and fix/refactor append and prepend methods

--- linkedList.js
+++ linkedList.js
@@ -8,44 +8,58 @@ class LinkedList {
       };
     };
     this.size = 0;
-    this.head = {
-      next: null,
-    };
+    this.head = null;
+    this.tail = null;
   }
 
   append(elem) {
+    let item = this.item(elem);
     if (this.size === 0) {
-      this.head.next = this.item(elem);
+      this.head = item;
+    } else {
+      this.tail.next = item;
     }
-    if (this.size === 1) {
-      this.head.next.next = this.item(elem);
-    }
-    if (this.size === 2) {
-      this.head.next.next.next = this.item(elem);
-    }
+
+    item.next = null;
+    this.tail = item;
     this.size += 1;
   }
 
   prepend(elem) {
     let item = this.item(elem);
-    item.next = this.head.next;
-    this.head.next = item;
+    if (this.size === 0) {
+      item.next = null;
+      this.tail = item;
+    } else {
+      item.next = this.head;
+    }
+    this.head = item;
     this.size += 1;
   }
   find(elem) {
-    if (this.head.next.value === elem) {
-      return 'elem was found!!!!';
-    } else {
-      return 'ERROR';
+    if (this.size === 0) {
+      return null;
     }
+
+    let searchItem = this.head;
+    while (searchItem.next) {
+      if (searchItem.value === elem) {
+        return searchItem;
+      } else {
+        searchItem = searchItem.next;
+      }
+    }
+
+    return null;
   }
   toArray() {
     let arrayFromList = [];
 
-    for (let i = 1; i <= this.size; i++) {
-      arrayFromList.push(this.head.next.value);
+    let searchItem = this.head;
+    while (searchItem.next) {
+      arrayFromList = [...arrayFromList, searchItem.value];
+      searchItem = searchItem.next;
     }
-
     return arrayFromList;
   }
 }
@@ -54,21 +68,31 @@ let list = new LinkedList();
 list.append('aaa');
 list.append('bbb');
 list.append('ccc');
+list.append('ddd');
+list.append('fff');
 
-console.log("list.find('aaa')");
-console.log(list.find('aaa'));
-
+console.warn('list.head');
+console.log(list.head);
 console.warn('list.head.next');
 console.log(list.head.next);
+console.warn('list.tail.next');
+console.log(list.tail.next);
 console.warn('list.head.next.next');
 console.log(list.head.next.next);
-console.warn('list.head.next.next.next');
-console.log(list.head.next.next.next);
 list.prepend('first');
+console.warn('list.head after prepend');
+console.log(list.head);
 console.warn('list.head.next after prepend');
 console.log(list.head.next);
 console.warn('list.head.next.next after prepend');
 console.log(list.head.next.next);
 console.log("list.find('first'))");
 console.log(list.find('first'));
+console.log("list.find('eee'))");
+console.log(list.find('eee'));
+console.log("list.find('ccc'))");
+console.log(list.find('ccc'));
+console.log('list.toArray()');
 console.log(list.toArray());
+
+module.exports = { LinkedList };
