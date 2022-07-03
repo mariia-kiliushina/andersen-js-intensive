412cbe15fc4f6646c6c0ea38442765c5cd5a265d
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-07-03 20:12:36 +0300

    with checks

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(
+  constructor({
     brand,
     model,
     yearOfManufacturing,
@@ -8,24 +8,54 @@ class Car {
     fuelConsumption,
     currentFuelVolume = 0,
     isStarted = false,
-    mileage = 0
-  ) {
-    this._brand = brand;
-    this._model = model;
-    this._yearOfManufacturing = yearOfManufacturing;
-    this._maxSpeed = maxSpeed;
-    this._maxFuelVolume = maxFuelVolume;
-    this._fuelConsumption = fuelConsumption;
-    this._currentFuelVolume = currentFuelVolume;
-    this._isStarted = isStarted;
-    this._mileage = mileage;
+    mileage = 0,
+  }) {
+    if (this._checkBrand(brand) === 'ok') {
+      this._brand = brand;
+    }
+    if (this._checkModel(model) === 'ok') {
+      this._model = model;
+    }
+    if (this._checkYear(yearOfManufacturing) === 'ok') {
+      this._yearOfManufacturing = yearOfManufacturing;
+    }
+    if (this._checkSpeed(maxSpeed) === 'ok') {
+      this._maxSpeed = maxSpeed;
+    }
+    if (this._checkMaxFuel(maxFuelVolume) === 'ok') {
+      this._maxFuelVolume = maxFuelVolume;
+    }
+    if (this._checkFuelConsumption(fuelConsumption) === 'ok') {
+      this._fuelConsumption = fuelConsumption;
+    }
+    if (this._checkCurrentFuelVolume(currentFuelVolume) === 'ok') {
+      this._currentFuelVolume = currentFuelVolume;
+    }
+    if (this._checkIsStarted(isStarted) === 'ok') {
+      this._isStarted = isStarted;
+    }
+    if (this._checkMileage(mileage) === 'ok') {
+      this._mileage = mileage;
+    }
   }
-  get brand() {
-    return this._brand;
+  _isNotValidNumber(variable) {
+    return (
+      typeof variable !== 'number' ||
+      isNaN(variable) ||
+      variable === Infinity ||
+      variable === -Infinity
+    );
   }
-  set brand(newBrand) {
+  _isNegativeOrZero(variable) {
+    return variable <= 0;
+  }
+
+  _throwTypeErrorWithText(type, variable, property) {
+    throw new Error(`Expected ${type} as ${property} instead got ${typeof variable}`);
+  }
+  _checkBrand(newBrand) {
     if (typeof newBrand !== 'string') {
-      throw `Expected string instead got ${typeof newBrand}`;
+      this._throwTypeErrorWithText('string', newBrand, 'brand');
     }
     if (newBrand === '') {
       throw 'The "brand" property cannot be empty';
@@ -33,14 +63,11 @@ class Car {
     if (newBrand.length > 50) {
       throw 'The "brand" property cannot be longer than 50 symbols';
     }
-    this._brand = newBrand;
+    return 'ok';
   }
-  get model() {
-    return this._model;
-  }
-  set model(newModel) {
+  _checkModel(newModel) {
     if (typeof newModel !== 'string') {
-      throw `Expected string instead got ${typeof newModel}`;
+      this._throwTypeErrorWithText('string', newModel, 'model');
     }
     if (newModel === '') {
       throw 'The "model" property cannot be empty';
@@ -48,51 +75,115 @@ class Car {
     if (newModel.length > 50) {
       throw 'The "model" property cannot be longer than 50 symbols';
     }
-    this.model = newModel;
+    return 'ok';
+  }
+  _checkYear(newYearOfManufacturing) {
+    if (this._isNotValidNumber(newYearOfManufacturing)) {
+      this._throwTypeErrorWithText('number', newYearOfManufacturing, 'yearOfManufacturing');
+    }
+    if (newYearOfManufacturing < 1900) {
+      throw 'The year of manufactoring cannot be earlier than 1900';
+    }
+    if (newYearOfManufacturing > new Date().getFullYear()) {
+      throw 'The year of manufactoring cannot be in the future';
+    }
+    return 'ok';
+  }
+  _checkSpeed(newMaxSpeed) {
+    if (this._isNotValidNumber(newMaxSpeed)) {
+      this._throwTypeErrorWithText('number', newMaxSpeed, 'maxSpeed');
+    }
+
+    if (newMaxSpeed < 100 || newMaxSpeed >= 300) {
+      throw 'Max speed should be within 100-300 km/h';
+    }
+    return 'ok';
+  }
+  _checkMaxFuel(newMaxFuelVolume) {
+    if (this._isNotValidNumber(newMaxFuelVolume)) {
+      this._throwTypeErrorWithText('number', newMaxFuelVolume, 'maxFuelVolume');
+    }
+
+    if (newMaxFuelVolume < 5 || newMaxFuelVolume >= 20) {
+      throw 'Max speed should be within 5-20 (not inclusively) liters';
+    }
+    return 'ok';
+  }
+  _checkFuelConsumption(newFuelConsumption) {
+    if (this._isNotValidNumber(newFuelConsumption)) {
+      this._throwTypeErrorWithText('number', newFuelConsumption, 'fuelConsumption');
+    }
+    if (this._isNegativeOrZero(newFuelConsumption)) {
+      throw new Error('Fuel consumption rate should be bigger than zero');
+    }
+    return 'ok';
+  }
+  _checkCurrentFuelVolume(currentFuelVolume) {
+    if (this._isNotValidNumber(currentFuelVolume)) {
+      this._throwTypeErrorWithText('number', currentFuelVolume, 'currentFuelVolume');
+    }
+    if (currentFuelVolume < 0) {
+      throw new Error('Current fuel volume should be a positive number');
+    }
+
+    return 'ok';
+  }
+  _checkIsStarted(isStarted) {
+    if (typeof isStarted !== 'boolean') {
+      this._throwTypeErrorWithText('boolean', isStarted, 'isStarted');
+    }
+    return 'ok';
+  }
+  _checkMileage(mileage) {
+    if (this._isNotValidNumber(mileage)) {
+      this._throwTypeErrorWithText('number', mileage, 'mileage');
+    }
+    if (mileage < 0) {
+      throw new Error('Mileage should be a positive number');
+    }
+    return 'ok';
+  }
+
+  get brand() {
+    return this._brand;
+  }
+  set brand(newBrand) {
+    this._checkBrand(newBrand);
+    this._brand = newBrand;
+  }
+  get model() {
+    return this._model;
+  }
+  set model(newModel) {
+    this._checkModel(newModel);
+    this._model = newModel;
   }
   get yearOfManufacturing() {
     return this._yearOfManufacturing;
   }
   set yearOfManufacturing(newYearOfManufacturing) {
-    if (typeof newYearOfManufacturing !== 'number') {
-      throw `Expected number instead got ${typeof newYearOfManufacturing}`;
-    }
-
-    if (newYearOfManufacturing < 1900) {
-      throw 'The year of manufactoring cannot be earlier than 1900';
-    }
+    this._checkYear(newYearOfManufacturing);
     this._newYearOfManufacturing = newYearOfManufacturing;
   }
   get maxSpeed() {
     return this._maxSpeed;
   }
   set maxSpeed(newMaxSpeed) {
-    if (typeof newMaxSpeed !== 'number') {
-      throw `Expected number instead got ${typeof newMaxSpeed}`;
-    }
-
-    if (newMaxSpeed < 100 || newMaxSpeed >= 300) {
-      throw 'Max speed should be within 100-300 km/h';
-    }
+    this._checkSpeed(newMaxSpeed);
     this._maxSpeed = newMaxSpeed;
   }
   get maxFuelVolume() {
     return this._maxFuelVolume;
   }
   set maxFuelVolume(newMaxFuelVolume) {
-    if (typeof newMaxFuelVolume !== 'number') {
-      throw `Expected number instead got ${typeof newMaxFuelVolume}`;
-    }
-
-    if (newMaxFuelVolume < 5 || newMaxFuelVolume >= 20) {
-      throw 'Max speed should be within 5-20 liters';
-    }
+    this.maxFuelVolume(newMaxFuelVolume);
     this._maxFuelVolume = newMaxFuelVolume;
   }
   get fuelConsumption() {
     return this._fuelConsumption;
   }
   set fuelConsumption(newFuelConsumption) {
+    this._checkFuelConsumption(newFuelConsumption);
     this._fuelConsumption = newFuelConsumption;
   }
   get currentFuelVolume() {
@@ -118,13 +209,10 @@ class Car {
     this._isStarted = false;
   }
   fillUpGasTank(liters) {
-    if (
-      typeof liters !== 'number' ||
-      liters === NaN ||
-      liters === Infinity ||
-      liters === -Infinity ||
-      liters <= 0
-    ) {
+    if (this._isNotValidNumber(liters)) {
+      throw new Error('Неверное количество топлива для заправки');
+    }
+    if (this._isNegativeOrZero(liters)) {
       throw new Error('Неверное количество топлива для заправки');
     }
     if (this._currentFuelVolume + liters > this._maxFuelVolume) {
@@ -133,32 +221,24 @@ class Car {
     this._currentFuelVolume += liters;
   }
   drive(speed, hours) {
-    if (
-      typeof speed !== 'number' ||
-      speed === NaN ||
-      speed === Infinity ||
-      speed === -Infinity ||
-      speed <= 0
-    ) {
+    if (this._isNotValidNumber(speed)) {
       throw new Error('Неверная скорость');
     }
 
-    if (
-      typeof hours !== 'number' ||
-      hours === NaN ||
-      hours === Infinity ||
-      hours === -Infinity ||
-      hours <= 0
-    ) {
+    if (this._isNegativeOrZero(speed)) {
+      throw new Error('Неверное количество топлива для заправки');
+    }
+
+    if (this._isNotValidNumber(hours)) {
       throw new Error('Неверное количество часов');
+    }
+
+    if (this._isNegativeOrZero(hours)) {
+      throw new Error('Неверное количество топлива для заправки');
     }
 
     if (speed > this._maxSpeed) {
       throw new Error('Машина не может ехать так быстро');
-    }
-
-    if (this._isStarted === false) {
-      throw new Error('Машина должна быть заведена, чтобы ехать');
     }
 
     if (this._isStarted === false) {
@@ -176,4 +256,18 @@ class Car {
   }
 }
 
+let myCar = new Car({
+  brand: 'asdsd',
+  model: 'hehehehhe',
+  yearOfManufacturing: 1967,
+  maxSpeed: 114,
+  maxFuelVolume: 14,
+  fuelConsumption: 76,
+  currentFuelVolume: 0,
+  isStarted: true,
+  mileage: 90,
+});
+
+console.log(myCar);
+console.log(myCar.);
 module.exports = { Car };
